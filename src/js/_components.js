d877a82ba4fb5b5870fcac48004b5e3bbe119e35
Анастасия Лемешко <an.zaschepenkova@yandex.ru>
2025-12-30 08:25:13 +0300

    корректировка подписей полей форм

--- src/js/_components.js
+++ src/js/_components.js
@@ -13,7 +13,6 @@ import { setViewToggles } from './components/_catalog-view.js';
 import { setSocialPopup } from './components/_popup-social.js';
 import { setModals } from './components/_modal.js';
 import { moveBlog } from './components/_move-blog.js';
-import { hideLabel } from './components/_hide-label.js';
 
 document.addEventListener('DOMContentLoaded', () => {
   toggleBurgerMenu();
@@ -33,5 +32,4 @@ document.addEventListener('DOMContentLoaded', () => {
   setSocialPopup();
   setModals();
   moveBlog();
-  hideLabel();
 });
