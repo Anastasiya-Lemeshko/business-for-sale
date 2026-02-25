0510ba02d06e5404de5473a5aa498586391ab65a
Анастасия Лемешко <an.zaschepenkova@yandex.ru>
2026-02-26 00:15:22 +0300

    удаление неиспользуемых файлов

--- src/js/components/_fancybox.js
+++ src/js/components/_fancybox.js
@@ -1,7 +1,6 @@
 import { Carousel } from '@fancyapps/ui';
 import { Fancybox } from '@fancyapps/ui';
 import { Lazyload } from '@fancyapps/ui/dist/carousel/carousel.lazyload.js';
-// import { Arrows } from '@fancyapps/ui/dist/carousel/carousel.arrows.js';
 import { Thumbs } from '@fancyapps/ui/dist/carousel/carousel.thumbs.js';
 
 const gallery = document.querySelector('.fancybox__gallery');
@@ -19,7 +18,6 @@ const setFancyboxGallery = () => {
     },
     {
       Lazyload,
-      // Arrows,
       Thumbs,
     }
   ).init();
